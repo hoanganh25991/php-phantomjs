d10762e94800a08dd056b4963fdc0a6498f65410
hoanganh25991 <lehoanganh25991@gmail.com>
2016-06-27 21:20:12 +0700

    phantomjs :+1, create a pdf file after reading through content of a webpage

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+namespace Redoc;
+require_once __DIR__ . "/vendor/autoload.php";
+use JonnyW\PhantomJs\Client;
+
+$client = Client::getInstance();
+
+/**
+ * @see JonnyW\PhantomJs\Http\PdfRequest
+ **/
+$request = $client->getMessageFactory()->createPdfRequest('https://github.com', 'GET');
+chdir("abc");
+$pdfFilePath = "document.pdf";
+
+$request->setOutputFile($pdfFilePath);
+$request->setFormat('A4');
+$request->setOrientation('landscape');
+$request->setMargin('1cm');
+
+/**
+ * @see JonnyW\PhantomJs\Http\Response
+ **/
+$response = $client->getMessageFactory()->createResponse();
+
+// Send the request
+/* setPath to phantomjs, this deal with Windows error not found executable file*/
+$phantomJsPath = __DIR__ . "/bin/phantomjs.exe";
+$client->getEngine()->setPath($phantomJsPath);
+$client->send($request, $response);
