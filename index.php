ee295ac1760e6960cee582b7d0cb912d56da8717
hoanganh25991 <lehoanganh25991@gmail.com>
2016-06-28 17:09:08 +0700

    remote bug asf

--- index.php
+++ index.php
@@ -1,37 +1,36 @@
 <?php
-namespace Redoc;
-require_once __DIR__ . "/vendor/autoload.php";
-use JonnyW\PhantomJs\Client;
-
-$client = Client::getInstance();
-
-echo "hello"; die;
-
-/**
- * @see JonnyW\PhantomJs\Http\PdfRequest
- **/
-$request = $client->getMessageFactory()->createPdfRequest('https://github.com', 'GET');
-chdir("abc");
-$pdfFilePath = "document.pdf";
-
-$request->setOutputFile($pdfFilePath);
-$request->setFormat('A4');
-$request->setOrientation('landscape');
-$request->setMargin('1cm');
-
-/**
- * @see JonnyW\PhantomJs\Http\Response
- **/
-$response = $client->getMessageFactory()->createResponse();
-
-// Send the request
-/* setPath to phantomjs, this deal with Windows error not found executable file*/
-$phantomJsPath = "/bin/phantomjs";
-if(DIRECTORY_SEPARATOR == "\\"){
-    $phantomJsPath = __DIR__ . "/bin/phantomjs.exe";
-}
-$client->getEngine()->setPath($phantomJsPath);
-$client->send($request, $response);
-
-echo "success";
-die;
+echo "hello";
+//namespace Redoc;
+//require_once __DIR__ . "/vendor/autoload.php";
+//use JonnyW\PhantomJs\Client;
+//
+//$client = Client::getInstance();
+//
+///**
+// * @see JonnyW\PhantomJs\Http\PdfRequest
+// **/
+//$request = $client->getMessageFactory()->createPdfRequest('https://github.com', 'GET');
+//chdir("abc");
+//$pdfFilePath = "document.pdf";
+//
+//$request->setOutputFile($pdfFilePath);
+//$request->setFormat('A4');
+//$request->setOrientation('landscape');
+//$request->setMargin('1cm');
+//
+///**
+// * @see JonnyW\PhantomJs\Http\Response
+// **/
+//$response = $client->getMessageFactory()->createResponse();
+//
+//// Send the request
+///* setPath to phantomjs, this deal with Windows error not found executable file*/
+//$phantomJsPath = "/bin/phantomjs";
+//if(DIRECTORY_SEPARATOR == "\\"){
+//    $phantomJsPath = __DIR__ . "/bin/phantomjs.exe";
+//}
+//$client->getEngine()->setPath($phantomJsPath);
+//$client->send($request, $response);
+//
+//echo "success";
+//die;
