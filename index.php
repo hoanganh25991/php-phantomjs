bb3f8b25e9996ac78ed0b4b9b6e46025be4f845e
hoanganh25991 <lehoanganh25991@gmail.com>
2016-06-29 09:07:26 +0700

    composer update, rollback to test on linux

--- index.php
+++ index.php
@@ -1,36 +1,37 @@
 <?php
-echo "hello";
-//namespace Redoc;
-//require_once __DIR__ . "/vendor/autoload.php";
-//use JonnyW\PhantomJs\Client;
-//
-//$client = Client::getInstance();
-//
-///**
-// * @see JonnyW\PhantomJs\Http\PdfRequest
-// **/
-//$request = $client->getMessageFactory()->createPdfRequest('https://github.com', 'GET');
-//chdir("abc");
-//$pdfFilePath = "document.pdf";
-//
-//$request->setOutputFile($pdfFilePath);
-//$request->setFormat('A4');
-//$request->setOrientation('landscape');
-//$request->setMargin('1cm');
-//
-///**
-// * @see JonnyW\PhantomJs\Http\Response
-// **/
-//$response = $client->getMessageFactory()->createResponse();
-//
-//// Send the request
-///* setPath to phantomjs, this deal with Windows error not found executable file*/
-//$phantomJsPath = "/bin/phantomjs";
-//if(DIRECTORY_SEPARATOR == "\\"){
-//    $phantomJsPath = __DIR__ . "/bin/phantomjs.exe";
-//}
-//$client->getEngine()->setPath($phantomJsPath);
-//$client->send($request, $response);
-//
-//echo "success";
-//die;
+namespace Redoc;
+require_once __DIR__ . "/vendor/autoload.php";
+use JonnyW\PhantomJs\Client;
+
+$client = Client::getInstance();
+var_dump("client instance");
+
+
+/**
+ * @see JonnyW\PhantomJs\Http\PdfRequest
+ **/
+$request = $client->getMessageFactory()->createPdfRequest('https://github.com', 'GET');
+chdir("abc");
+$pdfFilePath = "document.pdf";
+$request->setOutputFile($pdfFilePath);
+$request->setFormat('A4');
+$request->setOrientation('landscape');
+$request->setMargin('1cm');
+var_dump("set option");
+
+/**
+ * @see JonnyW\PhantomJs\Http\Response
+ **/
+$response = $client->getMessageFactory()->createResponse();
+
+// Send the request
+/* setPath to phantomjs, this deal with Windows error not found executable file*/
+$phantomJsPath = "/bin/phantomjs";
+if(DIRECTORY_SEPARATOR == "\\"){
+    $phantomJsPath = __DIR__ . "/bin/phantomjs.exe";
+}
+$client->getEngine()->setPath($phantomJsPath);
+
+/* get request, build reponse to pdf */
+$client->send($request, $response);
+var_dump("send request > build response");
